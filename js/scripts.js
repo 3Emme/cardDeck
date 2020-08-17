0443f0478d7cfcbcf965f2a4b1dc558f370572ca
Alma <a.q.frankenstein@gmail.com>
2020-08-17 14:35:01 -0700

    final version
    co-authored by: Drew Parker <drewjparker91@gmail.com>
    co-authored by: Ben Stoller <stollersystemsound@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,12 @@
 $(document).ready(function() {
-  const suits = ['hearts', 'diamonds', 'clovers', 'shovels'];
-  const cards = ['ace', 'king', 'queen', 'jack', '10', '9', '8', '7', '6', '5', '4', '3', '2'];
+  const suits = ['Hearts', 'Diamonds', 'Clovers', 'Shovels'];
+  const cards = ['Ace', 'King', 'Queen', 'Jack', '10', '9', '8', '7', '6', '5', '4', '3', '2'];
 
   let deck = [];
 
   suits.forEach(function(suit) {
     cards.forEach(function(card) {      
-      $("#deck").append("<li>",card + " of " + suit,"</li>")
+      $("#deck").append("<li>" + card + " of " + suit + "</li>")
     });
-  });
-
-
-  
+  });  
 });
